3f8d04d7e07de785b601ca67e2d38042e2dd3ae2
LHH <lhhuy@Komputer.local>
2023-05-20 01:06:17 +0700

    Upload demo project

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by LHH on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
